056adeead049bf666313e3934fcc34f6e7198085
Mina Gabriel <minamaged113@gmail.com>
2022-04-25 18:02:30 +0300

    add basic tests

--- tests/test_Repo.py
+++ tests/test_Repo.py
@@ -1,4 +1,21 @@
-from pygitr.Repo import add_one
+from pygitr.Repo import Repo
+from test_fixtures import TestEnv, use_empty_repo, use_basic_repo, use_repo_with_content
 
-def test_add_one():
-    assert add_one(1) == 2
+
+def test_Repo_class_exists():
+    repo = Repo()
+    assert isinstance(repo, Repo)
+
+def test_read_local_repository_from_path(use_basic_repo):
+    repo = Repo(path=use_basic_repo.path, remote=TestEnv.remote)
+    assert repo.path == use_basic_repo.path
+    assert repo.remote == TestEnv.remote
+
+def test_read_all_branches(use_repo_with_content):
+    repo = Repo(path=use_repo_with_content.path, remote=use_repo_with_content.remote)
+    assert "master" in repo.branches.keys()
+    assert "develop" in repo.branches.keys()
+
+def test_read_all_tags(use_repo_with_content):
+    repo = Repo(path=use_repo_with_content.path, remote=use_repo_with_content.remote)
+    assert TestEnv.test_tag in repo.tags.keys()
