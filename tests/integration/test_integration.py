173e020396bf3f5afe10455c33380c038c1ad86f
Mina Ghobrial <minamaged113@gmail.com>
2022-05-27 23:50:06 +0300

    add repo clone integration tests

---
+++ tests/integration/test_integration.py
@@ -0,0 +1,18 @@
+import os
+from pygitr.Repo import Repo
+
+
+def test_clone(tmpdir):
+    """Clone a repository from the web. and check the README content.
+
+    Args:
+        tmpdir (pytest.fixture): Pytest temporary directory.
+    """
+    repo = Repo(remote="https://github.com/xournalpp/xournalpp.git")
+    repo.path = tmpdir.strpath
+    repo.clone()
+    readme_filepath = os.path.join(repo.path,"README.md")
+    with open(readme_filepath, "r") as fh:
+        data = fh.read()
+
+    assert "# Xournal++" in data
