1f6c4096c5663435579a6f10cc77c1d04f668f0e
Mina Ghobrial <minamaged113@gmail.com>
2022-05-28 01:23:20 +0300

    add docstrings and change some references

--- src/pygitr/Repo.py
+++ src/pygitr/Repo.py
@@ -3,28 +3,31 @@ import subprocess
 import os
 from typing import Union
 
+
 class Repo:
-    b_repo_exists = False
+    """Repository representation in python."""
     remote = None
     path = None
     branches = None
     tags = None
 
-    def __init__(self, path: str=None, remote:str=None) -> None:
+    def __init__(self, path: str = None, remote: str = None) -> None:
         self.logger = logging.getLogger(f"{type(self).__name__}")
         self.logger.setLevel(logging.DEBUG)
 
         if remote:
-            self.logger.debug(f"creating a Repo object with remote set to: {remote}, "
-            f"and with path: {path}")
+            self.logger.debug(
+                f"creating a Repo object with remote set to: {remote}, "
+                f"and with path: {path}"
+            )
             self.remote = remote
             self.path = path
             self.logger.info(f"Repo object created with remote: {self.remote}")
             self.logger.info(f"Repo object created with remote: {self.path}")
         else:
             self.path = self.get_git_dir_path(path)
-            self.branches = self.get_branches()
-            self.tags = self.get_tags()
+            self.branches = self._get_branches()
+            self.tags = self._get_tags()
 
     def __repr__(self) -> str:
         """Provide string representation of Repo class.
@@ -35,10 +38,11 @@ class Repo:
         """
         return f"{self.remote}@{self.path}"
 
-    def clone(self, path:str=None) -> str:
+    def clone(self, path: str = None) -> str:
         """Clone remote repository into a given path.
         If the path is given, use the path to clone the repo into.
-        If the path is not given, check if the path is set into class variables.
+        If the path is not given, check if the path is set into
+        class variables.
 
         If the given path is not valid the command will fail.
 
@@ -47,17 +51,27 @@ class Repo:
 
         Returns:
             str: Path of the repository in the file system.
+
+        .. warning:: Side Effect: The path variable in the class instance will be set if the `path` variable is passed to the function.
+
         """
         if path:
             self.path = path
 
-        self.run(f"git clone {self.remote} .")
+        self._run(f"git clone {self.remote} .")
+        self.branches = self._get_branches()
+        self.tags = self._get_tags()
         return self.path
 
-    def checkout(self, ref) -> str:
-        self.run(f"git checkout {ref}")
+    def checkout(self, ref: str) -> None:
+        """Checkout a given branch in the current repo.
 
-    def get_git_dir_path(self, path:str) -> str:
+        Args:
+            ref (str): Specifies the commit reference.
+        """
+        self._run(f"git checkout {ref}")
+
+    def get_git_dir_path(self, path: str) -> str:
         """Get the root directory of the git repository.
 
         Args:
@@ -66,14 +80,14 @@ class Repo:
         Returns:
             str: path to the git directory containing `.git` directory.
         """
-        git_dir = self.run("git rev-parse --git-dir", path=path).stdout.decode().strip()
+        git_dir = self._run("git rev-parse --git-dir", path=path).stdout.decode().strip()
         if git_dir == ".git":
             # This is the root repository directory
             return path or os.getcwd()
         else:
             return git_dir.split(".git")[0]
 
-    def run(self, command: str, path:str=None) -> subprocess.CompletedProcess:
+    def _run(self, command: str, path: str = None) -> subprocess.CompletedProcess:
         return subprocess.run(
             command,
             cwd=path or self.path,
@@ -83,14 +97,13 @@ class Repo:
             check=True
         )
 
-    def get_branches(self) -> Union[dict,None]:
-        out = self.run(
+    def _get_branches(self) -> Union[dict, None]:
+        out = self._run(
             "git branch --format=\'%(refname);;%(refname:short);;%(objectname);;%(subject);;%(authorname);;%(authoremail);;%(authordate)\' -a"
         )
         raw_branches = out.stdout.decode("utf-8")
         if len(raw_branches) == 0:
             return None
-        
         dict_branches = {}
         branches = raw_branches.splitlines()
         for branch in branches:
@@ -107,8 +120,10 @@ class Repo:
 
         return dict_branches
 
-    def get_tags(self) -> Union[dict,None]:
-        out = self.run("git tag -l --format=\'%(refname);;%(refname:short);;%(objectname)\'")
+    def _get_tags(self) -> Union[dict, None]:
+        out = self._run(
+            "git tag -l --format=\'%(refname);;%(refname:short);;%(objectname)\'"
+        )
         raw_tags = out.stdout.decode("utf-8")
         if len(raw_tags) == 0:
             return None
