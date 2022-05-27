799a3a8e898902b984c6c27d3a0c0e9395a9331d
Mina Ghobrial <minamaged113@gmail.com>
2022-05-27 23:44:31 +0300

    add clone and checkout functionalities

--- src/pygitr/Repo.py
+++ src/pygitr/Repo.py
@@ -1,20 +1,71 @@
+import logging
 import subprocess
 import os
 from typing import Union
 
-class Repo():
+class Repo:
+    b_repo_exists = False
     remote = None
     path = None
     branches = None
     tags = None
 
     def __init__(self, path: str=None, remote:str=None) -> None:
-        self.path = self.get_git_dir_path(path)
-        self.remote = remote
-        self.branches = self.get_branches()
-        self.tags = self.get_tags()
+        self.logger = logging.getLogger(f"{type(self).__name__}")
+        self.logger.setLevel(logging.DEBUG)
 
-    def get_git_dir_path(self, path):
+        if remote:
+            self.logger.debug(f"creating a Repo object with remote set to: {remote}, "
+            f"and with path: {path}")
+            self.remote = remote
+            self.path = path
+            self.logger.info(f"Repo object created with remote: {self.remote}")
+            self.logger.info(f"Repo object created with remote: {self.path}")
+        else:
+            self.path = self.get_git_dir_path(path)
+            self.branches = self.get_branches()
+            self.tags = self.get_tags()
+
+    def __repr__(self) -> str:
+        """Provide string representation of Repo class.
+
+        Returns:
+            str: A string in the following format: 'Repo().remote@Repo().path'.
+                Will return '@' if neither Repo().remote or Repo().path is set.
+        """
+        return f"{self.remote}@{self.path}"
+
+    def clone(self, path:str=None) -> str:
+        """Clone remote repository into a given path.
+        If the path is given, use the path to clone the repo into.
+        If the path is not given, check if the path is set into class variables.
+
+        If the given path is not valid the command will fail.
+
+        Args:
+            path (str, optional): Path-like string. Defaults to None.
+
+        Returns:
+            str: Path of the repository in the file system.
+        """
+        if path:
+            self.path = path
+
+        self.run(f"git clone {self.remote} .")
+        return self.path
+
+    def checkout(self, ref) -> str:
+        self.run(f"git checkout {ref}")
+
+    def get_git_dir_path(self, path:str) -> str:
+        """Get the root directory of the git repository.
+
+        Args:
+            path (str): path string.
+
+        Returns:
+            str: path to the git directory containing `.git` directory.
+        """
         git_dir = self.run("git rev-parse --git-dir", path=path).stdout.decode().strip()
         if git_dir == ".git":
             # This is the root repository directory
